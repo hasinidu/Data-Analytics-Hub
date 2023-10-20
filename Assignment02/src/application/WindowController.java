ef3bfb501e1a7862332550e1b992048da71e44e9
hasin <hasin@10.0.0.22>
2023-10-20 22:23:53 +1100

    updates

--- Assignment02/src/application/WindowController.java
+++ Assignment02/src/application/WindowController.java
@@ -24,4 +24,13 @@ public class WindowController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void LoginWindow(ActionEvent event) throws IOException {
+
+		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 }
