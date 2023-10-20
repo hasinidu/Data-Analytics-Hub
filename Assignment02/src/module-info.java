32f26ce68309c9a7579d41510850b7df6e896e08
hasin <hasin@10.0.0.22>
2023-10-20 20:19:13 +1100

    New windows

--- Assignment02/src/module-info.java
+++ Assignment02/src/module-info.java
@@ -2,6 +2,7 @@ module Assignment02 {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
